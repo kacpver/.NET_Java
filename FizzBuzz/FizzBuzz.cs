8b4cb56c1c90e8cc4cb90e3e4f3678506105e616
kackub0352 <kackub0352@W04N-P317D15>
2024-02-28 19:57:51 +0100

    Add project files.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        int uB = 10;
+        public FizzBuzz(int uB)
+        {
+            this.uB = uB;
+        }
+        public void Display()
+        {
+
+            for (int i = 1; i< this.uB+1; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj limit:");
+            int uB = Convert.ToInt32(Console.ReadLine());
+        
+            FizzBuzz fb = new FizzBuzz(uB);
+            fb.Display();
+
+        }
+    }
+}
